ac4e8af13228bfbb1bd1613f53c52ebcad096c9d
kehongcheng <khc@ke123.com>
2022-02-10 11:19:40 +0800

    learn vue

---
+++ FrontEnd Development/Vue Preview/9. 工程化/my-vue-test/src/components/CourseAdd.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- 用户输入 -->
+  <div>
+    <p>
+      <input type="text" :value="value" @input="onInput" v-on:keydown.enter="addCourse" ref="inp">
+      <button @click="addCourse">add</button>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CourseAdd',
+    props: ['value'],
+      methods: {
+        addCourse() {
+          // 派发事件，通知父组件更新courses
+          this.$emit('add-course');
+        },
+        onInput(e) {
+          this.$emit("input", e.target.value);
+        }
+      },
+      mounted () {
+        this.$refs.inp.focus(); 
+      },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ FrontEnd Development/Vue Preview/9. 工程化/my-vue-test/src/components/Message.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="message-box" v-if="show">
+    <slot name="title"></slot>
+    <slot></slot>
+    <span class="message-box-close" @click="toggle">X</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Message',
+    data() {
+        return {
+          show: false
+        }
+      },
+      mounted () {
+        this.$bus.$on("message-close", () => {
+          this.toggle();
+        })
+      },
+      methods: {
+        toggle() {
+          this.show = !this.show;
+        }
+      },
+  }
+</script>
+
+<style scoped>
+.message-box {
+  padding: 10px 20px;
+}
+.message-box-close {
+  float: right;
+}
+.success {
+  background: #4fc08d;
+  border: 1px solid black;
+}
+.warning {
+  background: #EE9900;
+  border: 1px solid black;
+}
+</style>
